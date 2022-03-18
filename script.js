2e5257bca4c52f8b20a5d30c4b1a02c78df11cd0
BrunaSmaniotto <101889098+BrunaSmaniotto@users.noreply.github.com>
2022-03-18 13:59:23 -0300

    adicionei arquivos certificard

---
+++ script.js
@@ -0,0 +1,10 @@
+function mudaTema() {
+  document.body.classList.toggle("dark");
+}
+
+//Desafios:
+//Terminar todo o design que está no figma (disponível abaixo)
+//https://www.figma.com/file/1flmz2iauuNs8JsY6eaBHI/Imers%C3%A3o-Dev---Aula-9?node-id=0%3A1
+//Adicionar suas redes sociais de forma que quem clique consiga abrí-las
+
+//https://www.figma.com/file/I4p9ndfOKgjDKalFwd5ASr/Imers%C3%A3o-Dev---Aula-10?node-id=0%3A1
